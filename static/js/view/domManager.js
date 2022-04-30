dc89837bee726f238037ecc64f66b1b91ac3e07d
kisfelix <kisfelix1@gmail.com>
2022-04-30 21:47:02 +0200

    Refactor - extract appendChild

--- static/js/view/domManager.js
+++ static/js/view/domManager.js
@@ -1,26 +1,26 @@
 export let domManager = {
-  insertChildHTML(parentIdentifier, childHTML) {
-    const parent = document.querySelector(parentIdentifier);
-    if (parent) {
-      parent.insertAdjacentHTML("beforeend", childHTML);
-    } else {
-      console.error("could not find such html element: " + parentIdentifier);
-    }
-  },
-  appendChild(parentIdentifier, child) {
-    const parent = document.querySelector(parentIdentifier);
-    if (parent) {
-      parent.appendChild(child);
-    } else {
-      console.error("could not find such html element: " + parentIdentifier);
-    }
-  },
-  addEventListener(parentIdentifier, eventType, eventHandler) {
-    const parent = document.querySelector(parentIdentifier);
-    if (parent) {
-      parent.addEventListener(eventType, eventHandler);
-    } else {
-      console.error("could not find such html element: " + parentIdentifier);
-    }
-  },
+    insertChildHTML(parentIdentifier, childHTML) {
+        const parent = document.querySelector(parentIdentifier);
+        if (parent) {
+            parent.insertAdjacentHTML("beforeend", childHTML);
+        } else {
+            console.error("could not find such html element: " + parentIdentifier);
+        }
+    },
+    appendChild(parentIdentifier, child) {
+        const parent = document.querySelector(parentIdentifier);
+        if (parent) {
+            parent.appendChild(child);
+        } else {
+            console.error("could not find such html element: " + parentIdentifier);
+        }
+    },
+    addEventListener(parentIdentifier, eventType, eventHandler) {
+        const parent = document.querySelector(parentIdentifier);
+        if (parent) {
+            parent.addEventListener(eventType, eventHandler);
+        } else {
+            console.error("could not find such html element: " + parentIdentifier);
+        }
+    },
 };
